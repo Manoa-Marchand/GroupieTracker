5f04608b8f5973b833fd8e05ff3b784184416003
BAUMEUNIER <bastien.aumeunier@ynov.com>
2021-01-15 09:37:27 +0100

    ajout artistes

--- main.go
+++ main.go
@@ -1,6 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+type ArtistesJSON struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Relations    string   `json:"relations"`
+}
+
+var artistes []ArtistesJSON
 
 func main() {
 	fs := http.FileServer(http.Dir("./template/assets"))
@@ -8,8 +26,30 @@ func main() {
 
 	port := "8080"
 	http.HandleFunc("/", index)
+	http.HandleFunc("/Artists/", artists)
 	println("Le serveur se lance sur le port " + port)
 	http.ListenAndServe(":"+port, nil)
+
+}
+
+func artists(w http.ResponseWriter, r *http.Request) {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer res.Body.Close()
+	json.Unmarshal(data, &artistes)
+	tpl, err := template.ParseFiles("template/artist.html")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		tpl.Execute(w, &artistes)
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
